4e018e3660da4511219b5a99f7f3c554dacd9b1b
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-03-01 09:44:37 -0500

    trying to get the image to render from the local images folder

--- src/pages/CategoryList.js
+++ src/pages/CategoryList.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import data from '../data/categories.json'
 
 class CategoryList extends Component {
   render() {
+    let imageObject = Object.keys(data)
+    // let image = data.photos.pandas(this.props.match.params.i)
+    // console.log(image)
+    //return ( create variable to store the data - Object.keys(data) // persist appropriate data for image selected
+    // from photo list // dynamically show Image, Title of Image, and Description
     return (
       <div>
-        <h1>Interests</h1>
-        <h3>Categories</h3>
+        <h1>Things I Like</h1>
+        <main>
+          <figure>
+            <img alt="panda-image" src="../waving_panda.jpeg" />
+            <figcaption>
+              <h1>Title</h1>
+            </figcaption>
+          </figure>
+        </main>
         <Link to="/pandas">Enter!</Link>
       </div>
     )
--- src/pages/PhotoList.js
+++ src/pages/PhotoList.js
@@ -10,14 +10,14 @@ class PhotoList extends Component {
 
   render() {
     console.log(data)
-    let categories = Object.keys(data)
-    console.log(categories)
+    let pandas = Object.keys(data.pandas.photos)
+    console.log(pandas)
     return (
       <main>
         {data.pandas.photos.map((photo, i) => {
           return (
             <Link key={i} to={'./pandas' + i}>
-              <img src={photo.imageURL} title={photo.title} />
+              <img src={photo.imageURL} />
             </Link>
           )
         })}
